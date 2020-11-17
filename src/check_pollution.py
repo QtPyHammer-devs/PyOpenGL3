a55a8d86237ae19c9186f2b1f8bbaa1a93d71646
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-11-17 20:55:17 +1000

    more manual cleaning

--- src/check_pollution.py
+++ src/check_pollution.py
@@ -1,15 +1,10 @@
 #! /usr/bin/env python
 """Check for namespace polution..."""
 from __future__ import print_function
-
 current = set(dir())
-from OpenGL.GL import *
+from OpenGL.GL import *  # noqa: E402, F403, F401
 new = set(dir())
-unexpected = sorted([
-    x 
-    for x in (new-current-set(['current','OpenGL'])) 
-    if not x.lower().startswith( 'gl' )
-])
+unexpected = sorted([x for x in (new-current-set(["current", "OpenGL"])) if not x.lower().startswith("gl")])
 
 for name in unexpected:
     print(name)
