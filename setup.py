6af939a7dcd41a3d275c45ee3327ddfae54538f6
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-11-18 20:21:24 +1000

    finished cleaning up setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
-import sys, os
+from distutils.command.install_data import install_data
+import os
+import sys
 from setuptools import setup
 
 
@@ -18,32 +20,33 @@ metadata = dict(
     url="https://github.com/QtPyHammer-devs/PyOpenGL3",  # no builds to release yet
     download_url="https://github.com/QtPyHammer-devs/PyOpenGL3",  # PyPI release? contact mcfletch first
     keywords="Graphics, 3D, OpenGL, GLU, GLUT, GLE, GLX, EXT, ARB, Mesa, ctypes",
-    classifiers = ["License :: OSI Approved :: BSD License",
-                   "Programming Language :: Python",
-                   "Programming Language :: Python :: 3",
-                   "Topic :: Multimedia :: Graphics :: 3D Rendering",
-                   "Topic :: Software Development :: Libraries :: Python Modules",
-                   "Intended Audience :: Developers"],
+    classifiers=["License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python",
+                 "Programming Language :: Python :: 3",
+                 "Topic :: Multimedia :: Graphics :: 3D Rendering",
+                 "Topic :: Software Development :: Libraries :: Python Modules",
+                 "Intended Audience :: Developers"],
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
 
+
 def is_a_package(folder_path):
     return os.path.isfile(os.path.join(folder_path, "__init__.py"))
-    
+
+
 def packages_in(master_folder):
     """Find all packages under this directory"""
     out = list()
     for folder_path, directories, files in os.walk(master_folder):
         if is_a_package(folder_path):
-            out.append(path.replace("/", "."))
+            out.append(folder_path.replace("/", "."))
     return out
+
 
 requirements = []
 if sys.hexversion < 0x2050000:  # checking if python 3?
     requirements.append("ctypes")
-
-from distutils.command.install_data import install_data
 
 
 class smart_install_data(install_data):
@@ -54,13 +57,15 @@ class smart_install_data(install_data):
         # should create the directory if it doesn't exist!!!
         return install_data.run(self)
 
+
 extra_commands["install_data"] = smart_install_data
 
 if sys.platform == "win32":
     # binary versions of GLUT and GLE for Win32 (sigh)
     DLL_DIRECTORY = os.path.join("OpenGL", "DLLS")
     datafiles = [(DLL_DIRECTORY,
-    			  [os.path.join(DLL_DIRECTORY, file) for file in os.listdir(DLL_DIRECTORY) if os.path.isfile(os.path.join(DLL_DIRECTORY, file))])]
+                  [os.path.join(DLL_DIRECTORY, file) for file in os.listdir(DLL_DIRECTORY) if
+                   os.path.isfile(os.path.join(DLL_DIRECTORY, file))])]
 else:
     datafiles = []
 
@@ -69,7 +74,7 @@ if __name__ == "__main__":
     setup(name="PyOpenGL3",
           packages=packages_in("OpenGL"),
           description="Standard OpenGL bindings for Python",
-          options={"sdist": {"formats": ["gztar","zip"],
+          options={"sdist": {"formats": ["gztar", "zip"],
                              "force_manifest": True}},
           data_files=datafiles,
           cmdclass=extra_commands,
