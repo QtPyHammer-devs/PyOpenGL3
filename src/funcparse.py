363f09a6856ae07b781dbcd21e1e711b174b740f
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-12-01 10:30:20 +1000

    couple quick fixes

--- src/funcparse.py
+++ src/funcparse.py
@@ -7,6 +7,7 @@ log = logging.getLogger(__name__)
 
 reserved_names = set(keyword.kwlist)
 
+
 class Helper(object):
     def __getitem__(self, key):
         item = getattr(self, key, None)
@@ -17,6 +18,7 @@ class Helper(object):
         else:
             return item
 
+
 class Function(Helper):
     """Parse function parameters from C-style declaration"""
     def __init__(self, returnType, name, signature):
@@ -25,12 +27,14 @@ class Function(Helper):
         self.returnType = self.parseReturnType(returnType)
         try:
             self.argTypes, self.argNames = self.parseArguments(signature)
-        except Exception:
-            log.error("""Error parsing arguments for %s %s: %s""", name, signature, err)
+        except Exception as exc:
+            log.error(f"Error parsing arguments for {name} {signature}: {exc}")
             self.argTypes, self.argNames = (), ()
     findName = re.compile("[a-zA-z0-9]+$")
+
     def parseReturnType(self, returnType):
         return self.cTypeToPyType(returnType)
+
     def parseArguments(self, signature):
         """Parse a C argument-type declaration into a ctypes-style argTypes and argNames"""
         signature = signature.strip()
@@ -40,30 +44,31 @@ class Function(Helper):
         if not signature.strip() or signature.strip() == "void":
             return (), ()
         types, names = [], []
-        for i,item in enumerate(signature.split(",")):
+        for i, item in enumerate(signature.split(",")):
             # TODO: have to hack around the official header having junk here...
             if item.strip() == "EGLSyncKHR":
                 item = "EGLSyncKHR sync"
             item = item.strip()
             nameMatch = self.findName.search(item)
             if not nameMatch:
-                name = "arg_%i"%i
+                name = f"arg_{i}"
                 rest = item
             else:
                 name = nameMatch.group(0)
                 rest = item[:nameMatch.start(0)].strip()
                 if not rest:
-                    rest = name 
-                    name = "arg_%i"%i
+                    rest = name
+                    name = f"arg_{i}"
             if name in reserved_names:
                 name = name + "_"
             types.append(self.cTypeToPyType(rest))
             names.append(name)
         return types, names
     cTypeToPyType = staticmethod(ctype_to_pytype)
-    
+
     def errorReturn(self):
         return "0"
+
     def declaration(self):
         """Produce a declaration for this function in ctypes format"""
         returnType = self.returnType
@@ -75,21 +80,16 @@ class Function(Helper):
             argNames = ",".join(self.argNames)
         else:
             argNames = ""
-        arguments = ", ".join([
-            "%(type)s(%(name)s)"%locals()
-            for (type,name) in [
-                (type.split(".",1)[1],name)
-                for type,name in zip(self.argTypes,self.argNames)
-            ]
-        ])
-        name = self.name 
+        # arguments = ", ".join([f"{_type}({name})" for _type, name in [(type.split(".", 1)[1], name) for type, name in
+        #                                                               zip(self.argTypes, self.argNames)]])
+        name = self.name
         if returnType.strip() in ("_cs.GLvoid", "_cs.void"):
             returnType = pyReturn = "None"
         else:
             pyReturn = self.returnType
-        log.info("returnType %s -> %s", self.returnType, pyReturn)
-        doc = "%(name)s(%(arguments)s) -> %(pyReturn)s"%locals()
-        return self.TEMPLATE%locals()
-    TEMPLATE = """@_f
-@_p.types(%(returnType)s,%(argTypes)s)
-def %(name)s(%(argNames)s):pass"""
+        log.info(f"returnType {self.returnType} -> {pyReturn}")
+        # doc = f"{name}({arguments}) -> {pyReturn}"
+        return "\n".join(["@_f",
+                          f"@_p.types({returnType}, {argTypes})",
+                          f"def {name}({argNames}) -> {pyReturn}:",
+                          "    pass"])
